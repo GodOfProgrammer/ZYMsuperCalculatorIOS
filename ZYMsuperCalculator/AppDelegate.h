3f679f4483049b7d13e89710adcf4477656b4f2e
GodOfProgrammer <41535221+GodOfProgrammer@users.noreply.github.com>
2018-09-11 22:11:30 +0800

    Add files via upload

---
+++ ZYMsuperCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYMsuperCalculator
+//
+//  Created by Programmar on 18/8/6.
+//  Copyright © 2018年 Programmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYMsuperCalculator/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  bitch
+//
+//  Created by Programmar on 18/8/4.
+//  Copyright © 2018年 Programmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property(retain,nonatomic)UIButton *button;
+@property(retain,nonatomic)UILabel *label;
+@property(retain,nonatomic)NSMutableString *string;
+@property(assign,nonatomic)double num1,num2,num3,num4;
+@end
