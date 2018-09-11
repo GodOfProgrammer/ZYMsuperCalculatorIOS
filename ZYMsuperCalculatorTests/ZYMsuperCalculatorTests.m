f94f138764d296ba9f5e8b2e913706ec9e6c72c6
GodOfProgrammer <41535221+GodOfProgrammer@users.noreply.github.com>
2018-09-11 22:06:33 +0800

    Add files via upload

---
+++ ZYMsuperCalculatorTests/ZYMsuperCalculatorTests.m
@@ -0,0 +1,39 @@
+//
+//  ZYMsuperCalculatorTests.m
+//  ZYMsuperCalculatorTests
+//
+//  Created by Programmar on 18/8/6.
+//  Copyright © 2018年 Programmar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZYMsuperCalculatorTests : XCTestCase
+
+@end
+
+@implementation ZYMsuperCalculatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
